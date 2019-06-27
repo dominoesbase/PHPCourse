a2cfc311e92629b5ce03aa48e4516e948f51443f
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2019-06-27 10:31:13 +0100

    Tipos de Dados com Exemplos

---
+++ variaveis/exemplo-03.php
@@ -0,0 +1,38 @@
+<?php
+
+// TIPOS BASICOS
+
+$nome = "HCode";
+$site = "www.hcode.com.br";
+
+$ano = 1990;
+
+$salario = 5500.99;
+
+$bloqueado = false;
+
+//////////////////////////////
+
+// TIPOS COMPOSTOS
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+echo $frutas[2] . "</br>";
+
+$nascimento = new DateTime();
+
+var_dump($nascimento);
+
+echo "</br>";
+
+////////////////////////////
+
+//TIPOS ESPECIAIS
+
+$arquivo = fopen("exemplo-03.php", "r");
+
+var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
+
