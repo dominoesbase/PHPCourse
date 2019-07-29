fbe0f6e9bdcaa6dcb13c23cdead1d2660a62b21a
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2019-07-29 17:51:55 +0100

    Atualizado com operadores e strings (ainda nao esta terminado)

---
+++ operadores/exemplo-1.php
@@ -0,0 +1,24 @@
+<?php
+
+$nomeProprio = "Jorge";
+$nomeApelido = "Rodrigues";
+
+$nomeProprio .= " " . $nomeApelido;
+
+echo $nomeProprio; 
+echo "</br>";
+
+$valor1 = 2;
+$valor2 = 4;
+$valor2 += $valor1;
+
+echo $valor2;
+echo "</br>";
+
+$valorTotal = 25;
+$desconto = 0.15;
+$valorTotal -= ($valorTotal * $desconto);
+echo $valorTotal;
+echo "</br>";
+
+?>
