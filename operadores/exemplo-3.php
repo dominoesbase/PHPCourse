6a05be09f4ce19889bef07a8956926613bf2ded1
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2019-07-30 17:49:48 +0100

    Adicionados mais exemplos nos operadores e strings

--- operadores/exemplo-3.php
+++ operadores/exemplo-3.php
@@ -8,6 +8,17 @@ echo strtolower($nome);
 echo "</br>";
 echo ucwords($nome);
 echo "</br>";
+echo ucfirst($nome);
+echo "</br>";
+echo str_replace("rodrigues", "vida rodrigues", $nome);
+echo "</br>";
+echo $nome; // cuidado que a variavel nome nao foi modificada!
+echo "</br>";
+echo strpos($nome, "rodrigues");
+echo "</br>";
+echo substr($nome, strlen("jorge") + 1, strlen($nome)); // tentar realizar sem o +1 e vericar a diferenca :)
+
+
 
 
 
