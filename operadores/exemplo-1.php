c26cd47e0e5a746877a091a42e661e5bd7948d1d
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2019-07-30 17:08:57 +0100

    Ultimos exemplos de operadores e strings

--- operadores/exemplo-1.php
+++ operadores/exemplo-1.php
@@ -8,12 +8,16 @@ $nomeProprio .= " " . $nomeApelido;
 echo $nomeProprio; 
 echo "</br>";
 
+/////////////////////////////////////////////
+
 $valor1 = 2;
 $valor2 = 4;
 $valor2 += $valor1;
 
 echo $valor2;
 echo "</br>";
+
+/////////////////////////////////////////////
 
 $valorTotal = 25;
 $desconto = 0.15;
--- operadores/exemplo-2.php
+++ operadores/exemplo-2.php
@@ -1,4 +1,81 @@
 <?php
 
+$valor1 = 2;
+$valor2 = 5;
 
+echo $valor1 ** $valor2;
+echo "</br>";
+
+/////////////////////////////////////////////
+
+
+$valorInt1 = 5;
+$valorInt2 = 05;
+$valorString1 = "05";
+$valorString2 = "5";
+
+echo var_dump($valorInt1 == $valorInt2);
+echo "</br>"; 
+
+echo var_dump($valorInt1 == $valorString2);
+echo "</br>"; 
+
+echo var_dump($valorString1 === $valorString2);
+echo "</br>"; 
+
+echo var_dump($valorInt1 === $valorInt2);
+echo "</br>"; 
+
+/////////////////////////////////////////////
+
+$idadePedro = 19;
+$idadeCarlos = 21;
+
+echo $idadePedro <=> $idadeCarlos;
+echo "</br>"; 
+
+
+/////////////////////////////////////////////
+
+$nomeCao = NULL;
+$nomeGato = NULL;
+$nomeSapinho = "Tonto e Gordo";
+
+echo $nomeCao ?? $nomeGato ?? $nomeSapinho;
+echo "</br>"; 
+
+
+/////////////////////////////////////////////
+
+$nomeCasa = NULL;
+$nomePraia = NULL;
+$marcaCarro = NULL;
+
+$valorUsado = $nomeCasa ?? $nomePraia ?? $marcaCarro;
+
+if(isset($valorUsado)) {
+	echo "Valor usado para pesquisa: $valorUsado";
+	echo "</br>"; 
+} 
+else {
+	echo "Não foi usado qualquer valor válido para pesquisa! :(";
+	echo "</br>"; 
+}
+
+
+/////////////////////////////////////////////
+
+$idadeJorge = 21;
+echo $idadeJorge++;
+echo "</br>"; 
+echo $idadeJorge;
+echo "</br>"; 
+
+/////////////////////////////////////////////
+
+$idadeJorge = 21;
+echo ++$idadeJorge;
+echo "</br>"; 
+
+/////////////////////////////////////////////
 
