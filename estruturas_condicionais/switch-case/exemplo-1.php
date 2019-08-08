6ce1c38f427ba82e69be27ceed402972a0107118
Jorge Rodrigues <jorgemfvrodrigues@gmail.com>
2019-08-08 09:31:54 +0100

    Atualizado corretamente o exemplo do switch-case.

---
+++ estruturas_condicionais/switch-case/exemplo-1.php
@@ -0,0 +1,41 @@
+<?php
+
+// $diaDaSemana = date("w");
+$diaDaSemana = "8"; // para devolver o valor associado ao default
+
+switch ($diaDaSemana) {
+
+  case 0:
+  echo "Domingo";
+  break;
+
+  case 1:
+  echo "Segunda-Feira";
+  break;
+
+  case 2:
+  echo "Terça-Feira";
+  break;
+
+  case 3:
+  echo "Quarta-Feira";
+  break;
+
+  case 4:
+  echo "Quinta-Feira";
+  break;
+
+  case 5:
+  echo "Sexta-Feira";
+  break;
+
+  case 6:
+  echo "Sábado";
+  break;
+
+  default:
+  echo "Data Inválida";
+  break;
+}
+
+ ?>
